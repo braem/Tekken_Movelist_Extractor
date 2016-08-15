78c5af9070918428518b3b8223abc6935151a382
braem <bbraess@gmail.com>
2016-08-15 01:44:26 -0700

    Added Char datastructure for tekken characters

---
+++ src/extractor/Char.java
@@ -0,0 +1,20 @@
+package extractor;
+
+public class Char
+{
+	String id;
+	String name;
+	String fullname;
+	String sex;
+	String origin;
+	String fightingStyle;
+	
+	public Char(String id, String name, String fullname, String sex, String origin, String fightingStyle) {
+		this.id = id;
+		this.name = name;
+		this.fullname = fullname;
+		this.sex = sex;
+		this.origin = origin;
+		this.fightingStyle = fightingStyle;
+	}
+}
